325ab0b8239f49bb031aaa983adcf0f8827be78f
przrak <przrak@gmail.com>
2019-07-20 22:51:47 +0700

    second commit

--- src/main/java/com/udemy/auth/DBAuthenticator.java
+++ src/main/java/com/udemy/auth/DBAuthenticator.java
@@ -1,4 +1,22 @@
 package com.udemy.auth;
 
-public class DBAuthenticator {
+import com.google.common.base.Optional;
+import com.udemy.core.User;
+import com.udemy.db.UserDAO;
+import io.dropwizard.auth.AuthenticationException;
+import io.dropwizard.auth.Authenticator;
+import io.dropwizard.auth.basic.BasicCredentials;
+
+public class DBAuthenticator implements Authenticator<BasicCredentials, User> {
+
+    private UserDAO userDAO;
+
+    public DBAuthenticator(UserDAO userDAO) {
+        this.userDAO = userDAO;
+    }
+
+    @Override
+    public Optional<User> authenticate(BasicCredentials credentials) throws AuthenticationException {
+        return null;
+    }
 }
